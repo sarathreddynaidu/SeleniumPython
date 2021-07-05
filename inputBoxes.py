ce14e52859ceb1e2ee9339310f3d0a666d0aff1f
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-07-04 23:19:07 -0500

    updated input boxes file

--- inputBoxes.py
+++ inputBoxes.py
@@ -7,10 +7,18 @@ driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407")
 
 # Find number of input boxes
 inputBoxes = driver.find_elements(By.CLASS_NAME, "text_field")
-print(len(inputBoxes)) # 6
+print("Number of input boxes:", len(inputBoxes)) # 6
 
 # Provide value into the text box
-driver.find_element_by_id("RESULT_TextField-1").send_keys("Sarath Chandra Reddy")
-driver.find_element(By.ID, "RESULT_TextField-2").send_keys("Naidu")
+firstName = driver.find_element_by_id("RESULT_TextField-1")
+lastName = driver.find_element(By.ID, "RESULT_TextField-2")
+firstName.send_keys("Sarath Chandra Reddy")
+lastName.send_keys("Naidu")
+
+# Get status
+status = firstName.is_enabled()
+status1 = lastName.is_displayed()
+print("Status of first name: ", status)
+print("Status of second name: ", status1)
 
 # driver.quit()
