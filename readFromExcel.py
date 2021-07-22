d55024066b90d87a631e39f2c0e14ca0b0a4afd8
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-07-22 11:30:22 -0500

    add readFromExcel py file and SampleData excel file

---
+++ readFromExcel.py
@@ -0,0 +1,19 @@
+import openpyxl
+
+filePath = "C:\SeleniumPython\SampleData.xlsx"
+
+workbook = openpyxl.load_workbook(filePath)
+
+sheet = workbook.active
+# If there are multiple sheets use - workbook.get_sheet_by_name("Sheet1")
+
+rows = sheet.max_row
+columns = sheet.max_column
+
+print("Number of rows: ", rows)
+print("Number of columns: ", columns)
+
+for r in range(1, rows+1):
+    for c in range(1, columns+1):
+        print(sheet.cell(row=r, column=c).value, end="   ")
+    print()
